32ae9789daee797227480e576596bb1940ae492d
wardc10 <wardc10@go.stockton.edu>
2023-10-29 23:18:12 -0400

    updated birthday solver

--- Birthday/src/BirthdayChecker.java
+++ Birthday/src/BirthdayChecker.java
@@ -2,7 +2,6 @@ public class BirthdayChecker {
 	
 	public static void main(String args[]) {
 		Person checker = new Person();
-		System.out.println("Proability that: " + checker.birthday(365));
-		
+		System.out.println("Proability that: " + checker.birthday(10));
 	}
 }
--- Birthday/src/Person.java
+++ Birthday/src/Person.java
@@ -1,32 +1,19 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
 public class Person {
 
 	public Person() {
 		
 	}
 
-	Random rand = new Random();
-	public double birthday(int n) {
-		
-		List<Integer> l = new ArrayList<Integer>();
-		int shared = 0;
-		for(int i=0; i<n; i++) {
-			int r = rand.nextInt(365);
-			l.add(r);
-		}
-		for(int i=0; i<n; i++) {
-			for(int j=1; j<n-1; j++) {
-				if(l.get(i) == l.get(j)) {
-					shared = 1;
-					break;
-				}
+		public double birthday(int n) {
+			double shared_bday = 0;
+			double days = 365;
+			double count = 0;
+			for(int i=0; i<n; i++) {
+				shared_bday = shared_bday + (1/(days-count));
+				count++;
 			}
+			return shared_bday;
 		}
-		return shared/n;
-	}
 	
 	
 }
